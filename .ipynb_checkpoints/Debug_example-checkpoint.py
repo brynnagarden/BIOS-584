a15edf772e8c064bb7a343cace96a942fa30130d
brynnagarden <brynna.garden@emory.edu>
2025-10-31 09:59:35 -0400

    10/31

--- .ipynb_checkpoints/Debug_example-checkpoint.py
+++ .ipynb_checkpoints/Debug_example-checkpoint.py
@@ -5,7 +5,7 @@ from self_py_fun.DebugFun import *
 bp_low = 0.5
 bp_upp = 6
 electrode_num = 16
-parent_dir = '/Users/brynnagarden/Documents/GitHub/BIOS-584'
+parent_dir = '/Users/tma33/Library/CloudStorage/OneDrive-EmoryUniversity/Emory/Rollins SPH/2025/BIOS-584/python_proj'
 parent_data_dir = '{}/data'.format(parent_dir)
 time_index = np.linspace(0, 800, 25)
 electrode_name_ls = ['F3', 'Fz', 'F4', 'T7', 'C3', 'Cz', 'C4', 'T8', 'CP3', 'CP4', 'P3', 'Pz', 'P4', 'PO7', 'PO8', 'Oz']
@@ -30,9 +30,6 @@ eeg_trunc_type = np.squeeze(eeg_trunc_type, axis=1)
 [eeg_trunc_tar_mean, eeg_trunc_ntar_mean,
  eeg_trunc_tar_cov, eeg_trunc_ntar_cov,
  eeg_trunc_all_cov] = produce_trun_mean_cov(eeg_trunc_signal, eeg_trunc_type, electrode_num)
-
-print(eeg_trunc_ntar_mean.shape)
-print(eeg_trunc_ntar_mean)
 
 plot_trunc_mean(eeg_trunc_tar_mean, eeg_trunc_ntar_mean, subject_name, time_index, electrode_num, electrode_name_ls,
                 y_limit=np.array([-5, 8]))
